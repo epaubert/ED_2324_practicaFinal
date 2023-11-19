c6f32dcf48f350b09d6370dc2f470ebdaded0f99
Enrique Paubert Reca <epaubert97@gmail.com>
2023-11-19 19:28:12 +0100

    ./pruebapegado funciona correctamente al parecer

---
+++ pruebas/pegado.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./pruebapegado datos/imagenes/banderas/espana_reshigh.ppm datos/imagenes/aviones/avion2.ppm datos/imagenes/aviones/mascara_avion2.pgm pruebas/espana_avion2_blending.ppm 0 20 0
+
+feh pruebas/espana_avion2_blending.ppm
