132f28a90181039ed084b01d54849e48c8117b81
Enrique Paubert Reca <epaubert97@gmail.com>
2023-11-25 20:07:07 +0100

    arreglado pequeño error en Almacen_Rutas

--- rutas_aereas/include/Ruta.h
+++ rutas_aereas/include/Ruta.h
@@ -1,52 +1,53 @@
 #ifndef _RUTA_H_
 #define _RUTA_H_
 
-#include<iostream>
-#include<string>
-#include<Punto.h>
-#include<list>
+#include "../include/Punto.h"
+#include <iostream>
+#include <list>
+#include <string>
 
 using namespace std;
 
-class Ruta{
-    private:
-        list<Punto> puntos;
-        string code;
+class Ruta {
+private:
+  list<Punto> puntos;
+  string code;
 
-    public:
+public:
+  Ruta();
+  void Insertar(const Punto &n);
+  void Borrar(const Punto &n);
 
-        Ruta();
-        void Insertar(const Punto &n) ;
-        void Borrar(const Punto &n) ;
-        
-        string getCode() const ;
-        void setCode(const string &code) ;
+  string getCode() const;
+  void setCode(const string &code);
 
-        bool operator==(const Ruta &R) const ;
-        bool operator<(const Ruta &R) const ;
+  bool operator==(const Ruta &R) const;
+  bool operator<(const Ruta &R) const;
 
-        class iterator{
-            private:
-                list<Punto>:: iterator p;
-            public:
-        } ;
+  class iterator {
+  private:
+    list<Punto>::iterator p;
 
-        class const_iterator{
-            private:
-                list<Punto>::const_iterator p;
-            public:
-        };
+  public:
+  };
 
-        iterator begin() ;
-        const_iterator begin() const;
+  class const_iterator {
+  private:
+    list<Punto>::const_iterator p;
 
-        iterator end() ;
-        const_iterator end() const ;
+  public:
+  };
 
-        iterator find(const Punto &P) ;
+  iterator begin();
+  const_iterator begin() const;
 
-        friend istream &operator>>(istream &is, Ruta &R) ;
-        friend ostream &operator<<(ostream &os, Ruta &R) ;
- };
+  iterator end();
+  const_iterator end() const;
+
+  iterator find(const Punto &P);
+
+  friend istream &operator>>(istream &is, Ruta &R);
+  friend ostream &operator<<(ostream &os, Ruta &R);
+};
 
 #endif
