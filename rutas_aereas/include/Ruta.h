35c4ac195091fa2158e58555254edd1322b8eb21
Pedro Pelegrina Camarero <pedro.pelegrinacamarero@novatec-gmbh.de>
2023-11-25 20:02:43 +0100

    Archivo Ruta.h creado

---
+++ rutas_aereas/include/Ruta.h
@@ -0,0 +1,52 @@
+#ifndef _RUTA_H_
+#define _RUTA_H_
+
+#include<iostream>
+#include<string>
+#include<Punto.h>
+#include<list>
+
+using namespace std;
+
+class Ruta{
+    private:
+        list<Punto> puntos;
+        string code;
+
+    public:
+
+        Ruta();
+        void Insertar(const Punto &n) ;
+        void Borrar(const Punto &n) ;
+        
+        string getCode() const ;
+        void setCode(const string &code) ;
+
+        bool operator==(const Ruta &R) const ;
+        bool operator<(const Ruta &R) const ;
+
+        class iterator{
+            private:
+                list<Punto>:: iterator p;
+            public:
+        } ;
+
+        class const_iterator{
+            private:
+                list<Punto>::const_iterator p;
+            public:
+        };
+
+        iterator begin() ;
+        const_iterator begin() const;
+
+        iterator end() ;
+        const_iterator end() const ;
+
+        iterator find(const Punto &P) ;
+
+        friend istream &operator>>(istream &is, Ruta &R) ;
+        friend ostream &operator<<(ostream &os, Ruta &R) ;
+ };
+
+#endif
